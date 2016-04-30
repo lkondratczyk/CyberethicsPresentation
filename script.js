c521385d70abc3f347e7d9fefeed61a1dd0c6ddd
drew stiles <drewstiles@drews-MacBook.home>
2016-04-29 21:20:46 -0700

    Drew - Adds javascript support

---
+++ script.js
@@ -0,0 +1,42 @@
+/*
+ * drew's script
+ */ 
+
+
+window.onload = function() { 
+	var height = window.innerHeight;
+	var width = window.innerWidth;
+	
+	var root = document.getElementById("drew");
+	var secs = document.getElementsByClassName("drewSN");
+	var ptr = 0;
+	
+	root.style.height = (height - 80) + "px";
+	root.style.width = (width - 80) + "px";
+	
+	function next() {
+		ptr = (ptr + 1) % secs.length;
+		show(ptr);
+	}
+	
+	function show(p) {
+		console.log("p is " + p);
+		for (var i = 0; i < secs.length; i++) {
+			if (i == p) {
+				console.log("showing " + i);
+				secs[i].style.display = "block";
+			}
+			else {
+				secs[i].style.display = "none";
+			}
+		}
+	}
+	
+	document.getElementById("drewNxt").addEventListener("click", function() {
+		next();
+	});
+		
+};
+
+
+
